691c2ead69a197f6c6f18061dba009ac5f014047
samsonOdunola <odunolasamson@yahoo.com>
2022-06-19 11:56:49 +0100

    fixed grammer errors

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -9,10 +9,11 @@ const Mainpage = () => {
                 Front-End <br />
                 Developer
             </h1>
-            <p className="bio">I am Samson Odunola, a front-end developer based in Nigeria, who loves to create beautifull
-                and dynmaic user interfaces using tools like React, HTML, CSS, and JavaScript.
-                As a developer, i strongly believe there is tech solution to every problem and i am commited to
-                solving problems and creating a better user experience.
+            <p className="bio">I am Samson Odunola, a front-end developer based in Nigeria, who loves to create beautiful
+                and dynamic user interfaces using tools like React, HTML, CSS, and JavaScript.
+                As a developer, I strongly believe there is tech solution to every problem and i am committed to
+                solving problems and creating a better user experience
+
             </p>
             <button><Link className="aboutlink" to="/about">More about Me-</Link></button>
             
