c61a0aae66f68ea19ca3a47310934a5b9f9125df
samsonOdunola <odunolasamson@yahoo.com>
2022-11-04 22:10:03 +0100

    changed main title to full stack developer

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,28 +1,30 @@
-
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Work from "../components/Work";
 const Mainpage = () => {
-    return <>
-    <header>            
-            <p className="title">FREELANCE</p>
-            <h1 className="maintitle">
-                Front-End <br />
-                Developer
-            </h1>
-            <p className="bio">I am Samson Odunola, a front-end developer based in Nigeria, who loves to create beautiful
-                and dynamic user interfaces using tools like React, HTML, CSS, and JavaScript.
-                As a developer, I strongly believe there is tech solution to every problem and i am committed to
-                solving problems and creating a better user experience
-
-            </p>
-            <button><Link className="aboutlink" to="/about">More about Me-</Link></button>
-            
-    </header>
-    <Work/>
-    
-    </>;
-}
-
-
+  return (
+    <>
+      <header>
+        <p className="title">FREELANCE</p>
+        <h1 className="maintitle">
+          Full Stack <br />
+          Developer
+        </h1>
+        <p className="bio">
+          I am Samson Odunola, a full stack MERN developer based in Nigeria, who
+          loves to create beautiful and dynamic user interfaces using tools like
+          React, HTML, CSS, and JavaScript. As a developer, I strongly believe
+          there is tech solution to every problem and i am committed to solving
+          problems and creating a better user experience
+        </p>
+        <button>
+          <Link className="aboutlink" to="/about">
+            More about Me-
+          </Link>
+        </button>
+      </header>
+      <Work />
+    </>
+  );
+};
 
 export default Mainpage;
